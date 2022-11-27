6b808c25585b6a962929e15e11e9e859f55688d8
sıddık bayram <sddkbyrm2423@gmail.com>
2022-11-28 00:36:11 +0300

    chat events updated

--- src/rooms/ChatRoom.ts
+++ src/rooms/ChatRoom.ts
@@ -1,54 +1,140 @@
 import { Room } from "colyseus";
 
 export class ChatRoom extends Room {
-    // this room supports only 4 clients connected
-    maxClients = 30;
-    roomParticipants = [];
+  // this room supports only 4 clients connected
+  // maxClients = 30;
+  participants = {
+    global: {
+      roomName: "global",
+      participants: [],
+    },
 
-    onCreate(options) {
-        console.log("ChatRoom created!", options);
+  };
+  part = [];
 
-        this.onMessage("public-chat-message", (client, message) => {
-    
-            this.broadcast("new-public-chat-message", message);
+  onCreate(options) {
+    console.log("ChatRoom created!", options);
+
+    this.onMessage("send-room-chat-message", (client, message) => {
+      console.log({ room: message });
+
+      if (this.participants[message.roomName]) {
+        let roomUsers = this.participants[message.roomName].participants;
+        roomUsers.forEach((user) => {
+          let receiver = this.clients.filter(
+            (cl) => cl.sessionId === user.sessionId
+          );
+
+          if (receiver[0]) {
+            receiver[0].send("new-room-chat-message", message.message);
+          }
         });
+      }
+    });
 
-        this.onMessage("private-chat-message", (client, message) => {
-                
-            let receiver=this.clients.filter(cl => message.receiverSocketId===cl.sessionId);
-            receiver[0].send("new-private-chat-message",message.message);
-            
-        });
-        this.onMessage("new-client-joined", (client, participant) => {
+    this.onMessage("send-global-chat-message", (client, message) => {
+      this.broadcast("new-global-chat-message", message);
+    });
 
-            this.roomParticipants.push(participant);
-            this.broadcast("new-client-joined", this.roomParticipants);
-        });  
+    this.onMessage("public-chat-message", (client, message) => {
+      this.broadcast("new-public-chat-message", message);
+    });
 
-    }
+    this.onMessage("private-chat-message", (client, message) => {
+      let receiver = this.clients.filter(
+        (cl) => message.receiverSocketId === cl.sessionId
+      );
 
-    onJoin(client) {
-        // this.broadcast("messages", `${ client.sessionId } joined.`);
-    }
+      if (receiver[0]) {
+        receiver[0].send("new-private-chat-message", message.message);
+      }
+    });
+    this.onMessage("new-client-joined", (client, participant) => {
+      this.part.push(participant);
+      this.broadcast("new-client-joined", this.part);
+    });
 
-    async onLeave(client) {
-        // this.broadcast("messages", `${ client.sessionId } left.`);
-        this.roomParticipants = this.roomParticipants.filter(
-            (par) => par.id !== client.sessionId
-        );
+    this.onMessage("new-client-joined-room", (client, roomDetail) => {
+      if (!this.participants[roomDetail.roomName]) {
+        const newRoom = {
+          roomName: roomDetail.roomName,
+          participants: [],
+        };
+        const newRooms = {};
+        newRooms[roomDetail.roomName] = newRoom;
 
-        this.broadcast("new-client-joined", this.roomParticipants);
-        try {
-            await this.allowReconnection(client, 60);
-            console.log("Reconnected!");
+        this.participants = Object.assign({}, this.participants, newRooms);
 
-            client.send("status", "Welcome back!");
-        } catch (e) {
-            console.log(e);
+      }
+
+      if (this.participants[roomDetail.roomName]) {
+
+        let participants = this.participants[roomDetail.roomName].participants;
+        let isParticiantExist = participants.filter((item) => item.username === roomDetail.participant.username).length <= 0
+
+
+
+        if (isParticiantExist) {
+          this.participants[roomDetail.roomName].participants.push(
+            roomDetail.participant
+          );
         }
-    }
+      }
 
-    onDispose() {
-        console.log("Dispose ChatRoom");
+      this.broadcast("new-client-joined-room", this.participants);
+    });
+
+    this.onMessage("new-client-joined-global-room", (client, participant) => {
+      if (
+        this.participants["global"].participants.filter((item) => item.username === participant.username).length <= 0
+      ) {
+        this.participants["global"].participants.push(participant);
+      }
+
+      this.broadcast(
+        "new-client-joined-global-room",
+        this.participants["global"].participants
+      );
+    });
+  }
+
+  onJoin(client) {
+    // this.broadcast("messages", `${ client.sessionId } joined.`);
+  }
+
+  async onLeave(client) {
+
+    console.log({ client } + " Left room");
+
+   
+    this.part = this.part.filter((par) => par.id !== client.sessionId);
+
+
+
+    Object.keys(this.participants).flatMap(key => {
+      let room = this.participants[key];
+      let newParticpants = room.participants.filter((par) => par.sessionId !== client.sessionId);
+      this.participants[key].participants = newParticpants;
+      }
+    );
+
+
+    this.broadcast("client-left-room", this.participants);
+
+
+
+    this.broadcast("new-client-joined", this.part);
+    try {
+      await this.allowReconnection(client, 60);
+      console.log("Reconnected!");
+
+      client.send("status", "Welcome back!");
+    } catch (e) {
+      console.log(e);
     }
+  }
+
+  onDispose() {
+    console.log("Dispose ChatRoom");
+  }
 }
